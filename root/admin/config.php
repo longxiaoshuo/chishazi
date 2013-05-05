d9d3b1d14726b067013fc47dddcc858c9cbd205d
左岸 <itaofe@gmail.com>
2013-05-05 15:34:36 +0800

    first push

---
+++ root/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'H:\wordpress\EasyPHP-12.1\www\upload/admin/');
+define('DIR_SYSTEM', 'H:\wordpress\EasyPHP-12.1\www\upload/system/');
+define('DIR_DATABASE', 'H:\wordpress\EasyPHP-12.1\www\upload/system/database/');
+define('DIR_LANGUAGE', 'H:\wordpress\EasyPHP-12.1\www\upload/admin/language/');
+define('DIR_TEMPLATE', 'H:\wordpress\EasyPHP-12.1\www\upload/admin/view/template/');
+define('DIR_CONFIG', 'H:\wordpress\EasyPHP-12.1\www\upload/system/config/');
+define('DIR_IMAGE', 'H:\wordpress\EasyPHP-12.1\www\upload/image/');
+define('DIR_CACHE', 'H:\wordpress\EasyPHP-12.1\www\upload/system/cache/');
+define('DIR_DOWNLOAD', 'H:\wordpress\EasyPHP-12.1\www\upload/download/');
+define('DIR_LOGS', 'H:\wordpress\EasyPHP-12.1\www\upload/system/logs/');
+define('DIR_CATALOG', 'H:\wordpress\EasyPHP-12.1\www\upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'eshop');
+define('DB_PASSWORD', 'eshop');
+define('DB_DATABASE', 'eshop');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ root/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'H:\wordpress\EasyPHP-12.1\www\upload/catalog/');
+define('DIR_SYSTEM', 'H:\wordpress\EasyPHP-12.1\www\upload/system/');
+define('DIR_DATABASE', 'H:\wordpress\EasyPHP-12.1\www\upload/system/database/');
+define('DIR_LANGUAGE', 'H:\wordpress\EasyPHP-12.1\www\upload/catalog/language/');
+define('DIR_TEMPLATE', 'H:\wordpress\EasyPHP-12.1\www\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'H:\wordpress\EasyPHP-12.1\www\upload/system/config/');
+define('DIR_IMAGE', 'H:\wordpress\EasyPHP-12.1\www\upload/image/');
+define('DIR_CACHE', 'H:\wordpress\EasyPHP-12.1\www\upload/system/cache/');
+define('DIR_DOWNLOAD', 'H:\wordpress\EasyPHP-12.1\www\upload/download/');
+define('DIR_LOGS', 'H:\wordpress\EasyPHP-12.1\www\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'eshop');
+define('DB_PASSWORD', 'eshop');
+define('DB_DATABASE', 'eshop');
+define('DB_PREFIX', 'oc_');
+?>
